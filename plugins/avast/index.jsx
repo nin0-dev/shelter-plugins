82613e21458c6a236a86a902def3d46d1130bbab
nin0dev <personal@nin0dev.com>
2024-04-13 16:23:38 -0400

    discontinue

--- plugins/avast/index.jsx
+++ plugins/avast/index.jsx
@@ -8,9 +8,9 @@ export function onLoad() {
 shelter.http.intercept("POST", /\/channels\/\d+\/messages/, async (req, send) => {
 	// block discord posting dumb event
 	if (req.body.content == null) return;
-	req.body.content += "\n\n> 🔒 Secured by [Avast Antivirus Free](<https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html>).";
+	alert("Avast for Shelter has been discontinued. Please stop using it, its annoying");
+	//req.body.content += "\n\n> 🔒 Secured by [Avast Antivirus Free](<https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html>).";
 	const response = await send(req);
-	alert("Securing your message...\nPowered by Avast");
 	return response;
   });
 }
